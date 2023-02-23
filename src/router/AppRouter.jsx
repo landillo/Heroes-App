7feb8c7bb62053c0ff05646d68ad7adc32827c54
Orlando Ledón <orlando.leso@gmial.com>
2023-02-22 22:06:30 -0600

    Se dio formato a archivos

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,41 +1,40 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "../auth";
-import { HeroesRouters } from '../heroes/router/HeroesRouters';
-import { PrivateRouter } from './PrivateRouter';
-import { PublicRouter } from './PublicRouter.';
-
-
-
+import { HeroesRouters } from "../heroes/router/HeroesRouters";
+import { PrivateRouter } from "./PrivateRouter";
+import { PublicRouter } from "./PublicRouter.";
 
 export const AppRouter = () => {
   return (
     <>
-        <Routes>
-
-            {/*Rutas Publicas, son las rutas que queremos que se muestren cuando
+      <Routes>
+        {/*Rutas Publicas, son las rutas que queremos que se muestren cuando
               no esta autenticado el usuario, y se hacen en el PublicRouter y se ejecutan
               asi  */}
-            <Route path="/login" element={
-              <PublicRouter>
-                <LoginPage />
-              </PublicRouter>
-            } />
+        <Route
+          path="/login"
+          element={
+            <PublicRouter>
+              <LoginPage />
+            </PublicRouter>
+          }
+        />
 
-            {/*Rutas Privadas, son las rutas que queremos que se muestren cuando
+        {/*Rutas Privadas, son las rutas que queremos que se muestren cuando
               no esta autenticado el usuario, y se hacen en el PrivateRouter y se ejecutan
               asi  */}
-            <Route path="/*" element={
-              <PrivateRouter>
-                <HeroesRouters />
-              </PrivateRouter>
-            } />
+        <Route
+          path="/*"
+          element={
+            <PrivateRouter>
+              <HeroesRouters />
+            </PrivateRouter>
+          }
+        />
 
-
-            {/* <Route path="login" element={ <LoginPage /> } /> */}
-            {/* <Route path="/*" element={ <HeroesRouters /> } /> */}
-        
-        </Routes>
-
+        {/* <Route path="login" element={ <LoginPage /> } /> */}
+        {/* <Route path="/*" element={ <HeroesRouters /> } /> */}
+      </Routes>
     </>
-  )
-}
+  );
+};
